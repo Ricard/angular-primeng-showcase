318668099daddcba11a0570576db58beaac773ba
Ricard <ricard.castello@gmail.com>
2023-04-13 06:27:10 +0200

    feat: initial commit with first version of santalucia theme

---
+++ src/app/demo/showcase.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'showcase',
+  styleUrls: ['./showcase.component.scss'],
+  templateUrl: './showcase.component.html',
+})
+export class ShowcaseComponent {
+  value: string;
+}
